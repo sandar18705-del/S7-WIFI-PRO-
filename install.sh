cfef02b5979c6565e6b8b2ccee21e789de01a358
sandar18705-del <sandar18705@gmail.com>
2026-04-23 10:45:50 +0630

    install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,18 @@ rm -rf ~/s7wifi
 mkdir -p ~/s7wifi
 cd ~/s7wifi
 
-# ၃။ GitHub (sandar18705-del) ကနေ ဖိုင်တွေကို ဆွဲယူမယ်
-curl -LO https://raw.githubusercontent.com/sandar18705-del/S7-WIFI-PRO-/main/s7andar15.so
+# ၃။ ဖုန်းရဲ့ Bit (Architecture) ကို စစ်ဆေးမယ်
+ARCH=$(getprop ro.product.cpu.abi)
+
+if [ "$ARCH" = "arm64-v8a" ]; then
+    echo -e "\033[94m[*] 64-bit ဖုန်းဖြစ်ကြောင်း စစ်ဆေးတွေ့ရှိရသဖြင့် 64-bit version ကို ဒေါင်းလုဒ်ဆွဲနေသည်...\033[0m"
+    curl -LO https://raw.githubusercontent.com/sandar18705-del/S7-WIFI-PRO-/main/s7andar15_64bit.so -o s7andar15.so
+else
+    echo -e "\033[94m[*] 32-bit ဖုန်းဖြစ်ကြောင်း စစ်ဆေးတွေ့ရှိရသဖြင့် 32-bit version ကို ဒေါင်းလုဒ်ဆွဲနေသည်...\033[0m"
+    curl -LO https://raw.githubusercontent.com/sandar18705-del/S7-WIFI-PRO-/main/s7andar15.so -o s7andar15.so
+fi
+
+# ၄။ Starter ဖိုင်ကို ဒေါင်းမယ်
 curl -LO https://raw.githubusercontent.com/sandar18705-del/S7-WIFI-PRO-/main/s7andar15run.py
 
 echo -e "\033[92m[✔] အောင်မြင်စွာ ထည့်သွင်းပြီးပါပြီ!\033[0m"
